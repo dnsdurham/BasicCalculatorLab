cec342dfa69b6bc998d9099a68c83bb387aaafca
Doug Durham <ddurham@dontpaniclabs.com>
2020-01-28 15:59:55 -0600

    Added dewpoint

--- BasicCalculatorLab/BasicCalculator/Program.cs
+++ BasicCalculatorLab/BasicCalculator/Program.cs
@@ -6,19 +6,36 @@ namespace BasicCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("*** BASIC CALCULATOR ***");
+            Console.WriteLine("*** BASIC WEATHER CALCULATOR ***");
 
-            Console.WriteLine("Enter the first number");
+            // declare some variables
+            string input;
+            int temp;
+            int relHumdity;
+            int dewPoint;
 
-            // int.Parse will take a string data type and convert it to an int data type
-            int firstNumber = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter the second number");
-            int secondNumber = int.Parse(Console.ReadLine());
+            // Ask the user for the temp
+            Console.WriteLine("Enter a temp");
+            input = Console.ReadLine();
+            temp = int.Parse(input);
 
-            int sum = firstNumber + secondNumber;
+            // Ask them for rel humidity
+            Console.WriteLine("Enter RH");
+            input = Console.ReadLine();
+            relHumdity = int.Parse(input);
 
-            Console.WriteLine("The answer is " + sum);
+            // calculate the dew point
+            dewPoint = temp - 9 * (100 - relHumdity) / 25;
+
+            // print the dew point
+            Console.WriteLine("Dew point = " + dewPoint);
+
+            // ask for the windspeed
+
+            // calculate the wind chill
+
+            // print the wind chill
         }
     }
 }
